bef7c56ebda5cd73259d8b95b048eb45c3703892
Vladimir Dukhovoi <vdukhovoi@gmail.com>
2023-10-01 23:05:40 +0300

    Rework csharp and separate

---
+++ CSharpSeparated/TelemetrySystem/Tests/TelemetryDiagnosticControlsTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TelemetrySystem.Tests
+{
+    public class TelemetryDiagnosticControlsTest
+    {
+        [Test]
+        public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
+        {
+        }
+    }
+}
