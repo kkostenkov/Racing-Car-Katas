8200296ddb0561fc78d030c270568718e7f326b0
Kirill Kostenkov <kiril.kostenkov@tripledotfortune.com>
2023-10-03 20:33:54 +0200

    Add tests for TicketDispenser

--- CSharpSeparated/TurnTicketDispenser/Tests/TicketDispenserTests.cs
+++ CSharpSeparated/TurnTicketDispenser/Tests/TicketDispenserTests.cs
@@ -13,17 +13,33 @@ public class TicketDispenserTests
     {
         var ticketDispenser = new TicketDispenser();
         TurnTicket ticket = null;
-        for (int i = 0; i < ticketOrdinal; i++)
-        {
+        for (int i = 0; i < ticketOrdinal; i++) {
             ticket = ticketDispenser.GetTurnTicket();
         }
 
         return ticket.TurnNumber;
     }
 
+    [Test]
+    public void Should_DispenseDifferentTicketNumbers_When_MultipleDispensersAreUsed()
+    {
+        var ticketDispenser = new TicketDispenser();
+        var ticketDispenser1 = new TicketDispenser();
+        var ticketDispenser2 = new TicketDispenser();
+
+        var number = ticketDispenser.GetTurnTicket().TurnNumber;
+        var number1 = ticketDispenser1.GetTurnTicket().TurnNumber;
+        var number2 = ticketDispenser2.GetTurnTicket().TurnNumber;
+        
+        Assert.AreNotEqual(number, number1);
+        Assert.AreNotEqual(number1, number2);
+        Assert.AreNotEqual(number2, number);
+    }
+
     [TearDown]
     public void TearDown()
     {
-        typeof(TurnNumberSequence).GetField("_turnNumber", BindingFlags.Static | BindingFlags.NonPublic).SetValue(null, 0);
+        typeof(TurnNumberSequence).GetField("_turnNumber", BindingFlags.Static | BindingFlags.NonPublic)
+            .SetValue(null, 0);
     }
 }
