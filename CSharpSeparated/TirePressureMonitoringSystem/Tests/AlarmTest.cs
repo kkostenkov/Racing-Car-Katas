359251ebc5d0a14262c25a9f33ecdca224707983
Vladimir Dukhovoi <vdukhovoi@gmail.com>
2023-10-03 20:57:37 +0300

    Add simple test

--- CSharpSeparated/TirePressureMonitoringSystem/Tests/AlarmTest.cs
+++ CSharpSeparated/TirePressureMonitoringSystem/Tests/AlarmTest.cs
@@ -1,13 +1,31 @@
+using System.Reflection;
 using NUnit.Framework;
 
 namespace TDDMicroExercises.TirePressureMonitoringSystem
 {
     public class AlarmTest
     {
+        private Alarm alarm;
+        private Sensor fakeSensor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            alarm = new Alarm();
+            fakeSensor = new Sensor();
+        }
+        
+        [Test]
+        public void Should_BeOff_When_Created()
+        {
+            Assert.False(alarm.AlarmOn);
+        }
+
         [Test]
         public void Foo()
         {
-            Alarm alarm = new Alarm();
+            alarm.GetType().GetField("_sensor", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(alarm, fakeSensor);
+            alarm.Check();
             Assert.False(alarm.AlarmOn);
         }
     }
