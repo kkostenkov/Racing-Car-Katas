a3c8db7cb403b9cff5fd1106e450588156bcbce7
Vladimir Dukhovoi <vdukhovoi@gmail.com>
2023-10-03 21:29:05 +0300

    Add simple test

--- CSharpSeparated/TurnTicketDispenser/Tests/TicketDispenserTests.cs
+++ CSharpSeparated/TurnTicketDispenser/Tests/TicketDispenserTests.cs
@@ -1,13 +1,29 @@
+using System.Reflection;
 using NUnit.Framework;
+using TDDMicroExercises.TurnTicketDispenser;
 
 namespace Tests;
 
 public class TicketDispenserTests
 {
-    [Test]
-    public void foo()
+    [TestCase(1, ExpectedResult = 0)]
+    [TestCase(2, ExpectedResult = 1)]
+    [TestCase(100, ExpectedResult = 99)]
+    public int Should_DispenseTicketNumber_When_Created(int ticketOrdinal)
     {
-        Assert.IsFalse(true);
+        var ticketDispenser = new TicketDispenser();
+        TurnTicket ticket = null;
+        for (int i = 0; i < ticketOrdinal; i++)
+        {
+            ticket = ticketDispenser.GetTurnTicket();
+        }
+
+        return ticket.TurnNumber;
     }
-    
+
+    [TearDown]
+    public void TearDown()
+    {
+        typeof(TurnNumberSequence).GetField("_turnNumber", BindingFlags.Static | BindingFlags.NonPublic).SetValue(null, 0);
+    }
 }
